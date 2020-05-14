91ead826820a4b6b61dea61d2f9717a21647cdf5
Corinne Pulgar <corinne@corinnepulgar.com>
2020-05-14 14:41:48 -0400

    Multiple input

--- src/main/java/JDCompiler.java
+++ src/main/java/JDCompiler.java
@@ -16,34 +16,21 @@ public class JDCompiler {
 
     public static void main(String[] args) throws IOException {
         CommandLine cmd = setup(args);
-        String inputFilePath = cmd.getOptionValue("input");
-        String outputFilePath = cmd.getOptionValue("output");
+        String outputOption = cmd.getOptionValue("output");
 
-        if (!argsAreValid(inputFilePath, outputFilePath)) {
-            System.exit(1);
+        if (outputOption != null) {
+            if (!outputIsValid(outputOption)) {
+                System.exit(1);
+            }
         }
 
-        JustificationDiagram diagram = createDiagram(inputFilePath);
-        GraphDrawer drawer = new GraphDrawer();
-        StringBuilder gv = drawer.draw(diagram);
-
-        if (cmd.hasOption("gv")) {
-            String fileName = outputFilePath.split("\\.png")[0];
-            PrintWriter out = new PrintWriter(new FileWriter(fileName + ".gv"));
-            out.print(gv);
-            out.close();
+        for (int i = 0; i < cmd.getArgs().length; ++i) {
+            String inputFile = cmd.getArgs()[i];
+            if (!inputIsValid(inputFile)) {
+                continue;
+            }
+            generateFiles(cmd, cmd.getArgs()[i], generateOutputName(outputOption, i, inputFile));
         }
-
-        if (cmd.hasOption("td")) {
-            String fileName = outputFilePath.split("\\.png")[0];
-            PrintWriter out = new PrintWriter(new FileWriter(fileName + ".todo"));
-            RequirementsLister lister = new RequirementsLister();
-            out.print(lister.generate(diagram));
-            out.close();
-        }
-        InputStream dot = new ByteArrayInputStream(gv.toString().getBytes());
-        MutableGraph g = new guru.nidi.graphviz.parse.Parser().read(dot);
-        Graphviz.fromGraph(g).render(Format.PNG).toFile(new File(outputFilePath));
     }
 
     private static CommandLine setup(String[] args) {
@@ -51,16 +38,11 @@ public class JDCompiler {
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
 
-        Option input = new Option("i", "input", true, "input file path");
-        input.setRequired(true);
-        options.addOption(input);
-
         Option output = new Option("o", "output", true, "output file path");
-        output.setRequired(true);
         options.addOption(output);
 
-        options.addOption("gv", "output gv files");
-
+        options.addOption("png", "generate graph");
+        options.addOption("gv", "generate gv file");
         options.addOption("td", "generate todo list");
 
         try {
@@ -74,18 +56,55 @@ public class JDCompiler {
         }
     }
 
-    private static boolean argsAreValid(String in, String out) {
-        boolean valid = true;
+    private static void generateFiles(CommandLine cmd, String inputFilePath, String outputFilePath) throws IOException {
+        JustificationDiagram diagram = createDiagram(inputFilePath);
+        GraphDrawer drawer = new GraphDrawer();
+        StringBuilder gv = drawer.draw(diagram);
 
+        if (cmd.hasOption("png")) {
+            InputStream dot = new ByteArrayInputStream(gv.toString().getBytes());
+            MutableGraph g = new guru.nidi.graphviz.parse.Parser().read(dot);
+            Graphviz.fromGraph(g).render(Format.PNG).toFile(new File(outputFilePath + ".png"));
+        }
+        if (cmd.hasOption("gv")) {
+            PrintWriter out = new PrintWriter(new FileWriter(outputFilePath + ".gv"));
+            out.print(gv);
+            out.close();
+        }
+        if (cmd.hasOption("td")) {
+            PrintWriter out = new PrintWriter(new FileWriter(outputFilePath + ".todo"));
+            RequirementsLister lister = new RequirementsLister();
+            out.print(lister.generate(diagram));
+            out.close();
+        }
+    }
+
+    private static boolean inputIsValid(String in) {
         if (!in.matches(".*\\.(jd|txt)")) {
-            valid = false;
-            System.err.println("The input file should end with .jd or .txt");
+            System.err.println(in + " is not a valid name. The input file should end with .jd or .txt");
+            return false;
         }
-        if (!out.matches(".*\\.png")) {
-            valid = false;
-            System.err.println("The output file should end with .png");
+        return true;
+    }
+
+    private static String generateOutputName(String outputOption, int index, String inputOption) {
+        if (outputOption != null) {
+            if (index > 0) {
+                return outputOption + index;
+            } else {
+                return outputOption;
+            }
+        } else {
+            return inputOption.split("\\.")[0];
         }
-        return valid;
+    }
+
+    private static boolean outputIsValid(String out) {
+        if (out.matches(".*\\..*")) {
+            System.err.println("The output file should not have an extension");
+            return false;
+        }
+        return true;
     }
 
     public static JustificationDiagram createDiagram(String file) {
